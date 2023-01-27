d432ec16cd7177e28b99706daecb78c4dfef343c
deni2155 <createsoft15@gmail.com>
2023-01-27 22:41:09 +1100

    для дальнейшего использования

---
+++ AutoMainClassManifest/src/main/java/com/dreamer/automainclassmanifest/MainClass.java
@@ -0,0 +1,8 @@
+package com.dreamer.automainclassmanifest;
+
+public class MainClass {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ XMLParserTestMaven1/src/main/java/ru/scrooge/xmlparser/Main.java
@@ -0,0 +1,8 @@
+package ru.scrooge.xmlparser;
+
+public class Main{
+
+    public static void main(String[] args) {
+        System.out.print("Привет");
+    }
+}
